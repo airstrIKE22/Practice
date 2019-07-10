9ffce363c20c88d26880ab2e4dba730f0f0e316a
airtstrIKE22 <myke.kacy@protonmail.com>
2019-07-10 14:54:28 -0700

    updates

---
+++ asyncCalls.js
@@ -0,0 +1,12 @@
+/*function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    //instantiates it
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    //url goes in ajax info slot(browser url)
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+}*/
